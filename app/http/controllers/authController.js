5c65299b16245b1d4f2724ea4e6dddb0d521036d
sanket <sanketkothiya53@gmail.com>
2021-05-21 16:07:36 +0530

    add admin pannel and order place page

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -2,23 +2,22 @@ const User = require('../../models/user')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 function authController() {
+    const _getRedirectUrl = (req) => {
+        return req.user.role === 'admin' ? '/admin/orders' : '/customer/orders'
+    }
+    
     return {
         login(req, res) {
             res.render('auth/login')
         },
-        postLogin(req, res, next)
-         {
+        postLogin(req, res, next) {
             const { email, password }   = req.body
            // Validate request 
             if(!email || !password) {
                 req.flash('error', 'All fields are required')
                 return res.redirect('/login')
             }
-            // strategy local here we use
             passport.authenticate('local', (err, user, info) => {
-
-                // data come through passport.js 
-                // message is a kry
                 if(err) {
                     req.flash('error', info.message )
                     return next(err)
@@ -32,9 +31,10 @@ function authController() {
                         req.flash('error', info.message ) 
                         return next(err)
                     }
-                    return res.redirect('/')
+
+                    return res.redirect(_getRedirectUrl(req))
                 })
-            })(req, res, next)  //return function call here
+            })(req, res, next)
         },
         register(req, res) {
             res.render('auth/register')
@@ -42,7 +42,6 @@ function authController() {
         async postRegister(req, res) {
          const { name, email, password }   = req.body
          // Validate request 
-        //  console.log(req.body);
          if(!name || !email || !password) {
              req.flash('error', 'All fields are required')
              req.flash('name', name)
@@ -58,15 +57,14 @@ function authController() {
                 req.flash('email', email) 
                 return res.redirect('/register')
              }
-         }) 
+         })
 
          // Hash password 
          const hashedPassword = await bcrypt.hash(password, 10)
          // Create a user 
          const user = new User({
-            //  also write singe like name
-             name:name,
-             email:email,
+             name,
+             email,
              password: hashedPassword
          })
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 import Noty from 'noty'
+import { initAdmin } from './admin'
 
 let addToCart = document.querySelectorAll('.add-to-cart')
 let cartCounter = document.querySelector('#cartCounter')
 
 function updateCart(pizza) {
    axios.post('/update-cart', pizza).then(res => {
-    
-   cartCounter.innerText = res.data.totalQty
-   console.log(res.data.totalQty);
+       cartCounter.innerText = res.data.totalQty
        new Noty({
            type: 'success',
            timeout: 1000,
@@ -23,13 +22,21 @@ function updateCart(pizza) {
            progressBar: false,
        }).show();
    })
-   
 }
 
 addToCart.forEach((btn) => {
    btn.addEventListener('click', (e) => {
        let pizza = JSON.parse(btn.dataset.pizza)
-       console.log(pizza);
        updateCart(pizza)
    })
 })
+
+// Remove alert message after X seconds 
+const alertMsg = document.querySelector('#success-alert')
+if(alertMsg) {
+   setTimeout(() => {
+       alertMsg.remove()
+   }, 2000)
+}
+
+initAdmin()
